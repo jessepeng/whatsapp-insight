a579cd115b28bae0ca40d19e610d02d62f1d4a9d
Jan-Christopher Pien <101@janchristopherpien.de>
2016-11-04 23:42:28 +0100

    Neue Befehle

--- src/main/java/de/jessepeng/WhatsappInsights/Main.java
+++ src/main/java/de/jessepeng/WhatsappInsights/Main.java
@@ -4,10 +4,7 @@ import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.io.TextValueInputFormat;
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.api.java.tuple.Tuple3;
-import org.apache.flink.api.java.tuple.Tuple4;
-import org.apache.flink.api.java.tuple.Tuple5;
+import org.apache.flink.api.java.tuple.*;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.core.fs.Path;
@@ -16,14 +13,12 @@ import org.apache.flink.types.StringValue;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -48,12 +43,23 @@ public class Main {
     public static final String DELIM = " \t\n\r\f.,:;-!?\"'()“”";
     public static final String COMMAND_NACHRICHTEN_ABSTAND = "nachrichtenAbstand";
     public static final String COMMAND_NACHRICHTEN_PRO_TAG = "nachrichtenProTag";
+    public static final String COMMAND_WORT_DURCHSCHNITT_STUNDE = "wortDurchschnittStunde";
+    public static final String COMMAND_WORT_DURCHSCHNITT_TAG = "wortDurchschnittTag";
+    public static final String COMMAND_WORT_DURCHSCHNITT_SENDER = "wortDurchschnittSender";
 
     public static void main(String[] args) throws Exception {
         ParameterTool parameter = ParameterTool.fromArgs(args);
         String datei = parameter.getRequired("eingabe");
         String ausgabe = parameter.getRequired("ausgabe");
         String command = parameter.getRequired("befehl");
+
+        Map<String, String> nummerMap = new HashMap<>();
+        nummerMap.put("bombig", "Benjamin Tschierschke");
+        nummerMap.put("\u202A+90 553 157 0708\u202C", "Vicky Tettling");
+        nummerMap.put("\u202A+49 176 55384961\u202C", "Vicky Tettling");
+        nummerMap.put("\u202A+49 176 56726229\u202C", "Ronja Wiesenthal");
+        nummerMap.put("\u202A+49 1516 4966879\u202C", "Nico Puchinger");
+        nummerMap.put("Josefine Gröhst", "Josephine Gröhst");
 
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
@@ -64,6 +70,7 @@ public class Main {
             if (nachrichtMatcher.matches()) {
                 Date datum = new SimpleDateFormat("dd.MM.yy, hh:mm:ss").parse(nachrichtMatcher.group(1));
                 String von = nachrichtMatcher.group(2);
+                von = nummerMap.getOrDefault(von, von);
                 String nachricht = nachrichtMatcher.group(3);
                 NachrichtenTyp nachrichtenTyp;
                 switch (nachricht) {
@@ -133,7 +140,6 @@ public class Main {
                         out.collect(new Tuple2<String, List<String>>(tuple.f1, woerter));
                     }
                 });
-
                 DataSet<Tuple3<String, Integer, Integer>> nachrichtenLaenge = wortListe.map((tuple) -> new Tuple3<String, Integer, Integer>(tuple.f0, tuple.f1.size(), 1));
                 resultDataset = nachrichtenLaenge
                         .groupBy(0)
@@ -166,6 +172,58 @@ public class Main {
                     return new Tuple2<String, Integer>(dateFormat.format(tuple.f0), 1);
                 }).groupBy(0).sum(1).sortPartition(1, Order.DESCENDING);
                 break;
+            case COMMAND_WORT_DURCHSCHNITT_STUNDE:
+                int numberOfDays = Integer.valueOf(parameter.getRequired("tage"));
+                resultDataset = nachrichten
+                    .map(tuple -> {
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMYYYYHH");
+                        return new Tuple3<String, Date, Integer>(dateFormat.format(tuple.f0), tuple.f0, 1);
+                    })
+                    .groupBy(0)
+                    .sum(2)
+                    .map(tuple -> {
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("HH");
+                        return new Tuple2<String, Integer>(dateFormat.format(tuple.f1), tuple.f2);
+                    })
+                    .groupBy(0)
+                    .reduce((tuple1, tuple2) -> new Tuple2<String, Integer>(tuple1.f0, tuple1.f1 + tuple2.f1))
+                    .map(tuple -> new Tuple2<String, Double>(tuple.f0, new BigDecimal(((double) tuple.f1 / numberOfDays)).round(new MathContext(4)).doubleValue()));
+                break;
+            case COMMAND_WORT_DURCHSCHNITT_TAG:
+                resultDataset = nachrichten
+                        .map(tuple -> {
+                            DateFormat dateFormat = new SimpleDateFormat("ddMMYYYY");
+                            return new Tuple3<String, Date, Integer>(dateFormat.format(tuple.f0), tuple.f0, 1);
+                        })
+                        .groupBy(0)
+                        .sum(2)
+                        .map(tuple -> {
+                            Calendar calendar = new GregorianCalendar();
+                            calendar.setTime(tuple.f1);
+                            return new Tuple3<String, Integer, Integer>(String.valueOf(calendar.get(Calendar.DAY_OF_WEEK)), tuple.f2, 1);
+                        })
+                        .groupBy(0)
+                        .reduce((tuple1, tuple2) -> new Tuple3<String, Integer, Integer>(tuple1.f0, tuple1.f1 + tuple2.f1, tuple1.f2 + tuple2.f2))
+                        .map(tuple -> {
+                            return new Tuple2<String, Double>(tuple.f0, new BigDecimal(((double) tuple.f1 / tuple.f2)).round(new MathContext(4)).doubleValue());
+                        });
+                break;
+            case COMMAND_WORT_DURCHSCHNITT_SENDER:
+                numberOfDays = Integer.valueOf(parameter.getRequired("tage"));
+                resultDataset = nachrichten
+                        .map(tuple -> {
+                            DateFormat dateFormat = new SimpleDateFormat("ddMMYYYY");
+                            return new Tuple4<String, String, Date, Integer>(tuple.f1, dateFormat.format(tuple.f0), tuple.f0, 1);
+                        })
+                        .groupBy(0, 1)
+                        .sum(3)
+                        .map(tuple -> new Tuple2<String, Integer>(tuple.f0, tuple.f3))
+                        .groupBy(0)
+                        .sum(1)
+                        .map(tuple -> {
+                            return new Tuple2<String, Double>(tuple.f0, new BigDecimal(((double) tuple.f1 / numberOfDays)).round(new MathContext(4)).doubleValue());
+                        });
+                break;
             default:
                 System.out.println("Befehl nicht erkannt.");
                 System.exit(-1);
@@ -173,7 +231,11 @@ public class Main {
         }
 
         env.setParallelism(1);
-        resultDataset.writeAsText(ausgabe, FileSystem.WriteMode.OVERWRITE);
+        if (parameter.get("format", "txt").equalsIgnoreCase("csv")) {
+            resultDataset.writeAsCsv(ausgabe, FileSystem.WriteMode.OVERWRITE);
+        } else {
+            resultDataset.writeAsText(ausgabe, FileSystem.WriteMode.OVERWRITE);
+        }
 
         env.execute();
     }
